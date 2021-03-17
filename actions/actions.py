65a3a7330c565ea3970487a0318a3e5d9f1a6243
rajendra-a <rajendrareddy.akkala@gmail.com>
2021-03-17 11:14:17 +0530

    added email sending option

--- actions/actions.py
+++ actions/actions.py
@@ -13,7 +13,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
-from rasa_sdk.events import Slotset
+from rasa_sdk.events import SlotSet
 
 
 class ActionSendEmail(FormAction):
@@ -21,29 +21,35 @@ class ActionSendEmail(FormAction):
     def name(self) -> Text:
         return "action_send_email"
 
+    def required_slots():
+        return ['username', 'email-address', 'phone-number']
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         username = tracker.get_slot("username")
-        email_address = tracker.get_slot("email_address")
+        email_address = tracker.get_slot("email-address")
         phone_number = tracker.get_slot("phone-number")
 
-        smtp_server = "smtp.syberzen.com"
+        smtp_server = "smtp.gmail.com"
         port = 465
-        sender_email_address = "rajendra@syberzen.com"
+        sender_email_address = "rajendrareddy.akkala@gmail.com"
         receiver_email_address = "rajendra@syberzen.com"
-        password = "Rajendra@sz"
+        password = "Rajendra@11*#"
 
-        message = f"This message sent from chatbot user name is {username},email address {email-address}\n and phone number {phone-number}"
+        message = "This message sent from chatbot user name is {}, email address is {} and phone number is {}".format(username, email_address, phone_number)
 
         context = ssl.create_default_context()
 
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, receiver_email, message)
-            server.sendmail(sender_email, receiver_email, message)
+            server.login(sender_email_address, password)
+            server.sendmail(sender_email_address, receiver_email_address, message)
         
 
         dispatcher.utter_message(template="utter_contact_you")
 
-        return []
+        return [SlotSet(username,None),
+                SlotSet(email_address,None),
+                SlotSet(phone_number,None)]
+        
